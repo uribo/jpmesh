209009944f5c2bfd5a2775f275c9c4ed6d3ad664
Shinya Uryu <suika1127@gmail.com>
2016-09-24 10:36:36 +0900

    add test code

--- tests/testthat/test-mesh_area.R
+++ tests/testthat/test-mesh_area.R
@@ -43,4 +43,19 @@ test_that("eight mesh", {
   expect_equal(res$lat2, 34.68542, tolerance = .002)
 })
 
-
+# Misc --------------------------------------------------------------------
+test_that("eight mesh", {
+  
+  res <- c(5135749624, 5135749642, 5135749644) %>%
+    purrr::map(mesh_area, order = "quarter") %>%
+    purrr::map_df(~ .[c("lng1", "lat1", "lng2", "lat2")])
+  res$mesh <- c(5135749624, 5135749642, 5135749644)
+  
+  expect_true(is.data.frame(res))
+  expect_equal(names(res), c("lng1", "lat1", "lng2", "lat2", "mesh"))
+  
+  expect_equal(res$lng1[1], res$lng1[2], tolerance = .002)
+  expect_equal(res$lng2[1], res$lng2[2], tolerance = .002)
+  expect_equal(res$lat1[2], res$lat2[3], tolerance = .002)
+  
+})
--- tests/testthat/test-meshcode_to_latlon.R
+++ tests/testthat/test-meshcode_to_latlon.R
@@ -35,3 +35,16 @@ test_that("Calculate correct value for mesh_code (1km)", {
   expect_equal(res$lat_center - res$lat_error, 34.65)
   expect_equal(res$long_center - res$long_error, 133.9125)
 })
+
+# Misc. --------------------------------------------------
+test_that("Combine other function", {
+  res <- latlong_to_meshcode(34.688732, 135.527193) %>% 
+    meshcode_to_latlon()
+  
+  expect_that(res, is_a("data.frame"))
+  expect_equal(names(res), c("lat_center", "long_center", "lat_error", "long_error"))
+  
+  expect_equal(res$lat_center - res$lat_error, 34.68333, tolerance = .002)
+  expect_equal(res$long_center - res$long_error, 135.525)
+
+})
