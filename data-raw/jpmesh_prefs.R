a0b8a737486b390e79b7bb272986c1e476383492
Shinya Uryu <suika1127@gmail.com>
2017-07-28 09:30:53 +0900

    bind

--- data-raw/jpmesh_prefs.R
+++ data-raw/jpmesh_prefs.R
@@ -6,9 +6,18 @@ library(dplyr)
 library(foreach)
 library(jpmesh)
 
-df.mesh.csv <- list.files("data-raw/city_mesh", full.names = TRUE) %>% data_frame(file = .)
+library(rvest)
+base.url <- "http://www.stat.go.jp/data/mesh/"
+x <- read_html(paste0(base.url, "m_itiran.htm"))
+links <- x %>% html_nodes(xpath = '//*[@id="contents"]/ul/li/a') %>%
+  html_attr("href")
 
-jpmesh.pref <- df.mesh.csv %>% 
+foreach(i = 1:length(links)) %do% {
+  download.file(paste0(base.url, links[i]),
+                destfile = paste0("~/Dropbox/city_mesh/", gsub(".+/", "",  links[i])))
+}
+
+jpmesh.pref <- df.mesh.csv[1] %>% 
   group_by(file) %>% 
   do(res = jpmesh:::raw_pref_mesh(.$file)) %>% 
   tidyr::unnest() %>% 
@@ -22,5 +31,10 @@ jpmesh.pref <- df.mesh.csv %>%
 foreach(i = 1:length(unique(jpmesh.pref$jiscode))) %do% {
   jpmesh.pref %>% 
     filter(jiscode == sprintf("%02s", i)) %>%
-    readr::write_rds(path = paste0("inst/extdata/pref_", sprintf("%02s", i), "_mesh.rds"))
+    readr::write_rds(path = paste0("inst/extdata/pref_", sprintf("%02s", i), "_mesh.rds"), compress = "xz")
 }
+
+
+readr::read_rds(paste0("~/git/r_pkg/jpmesh/inst/extdata/pref_", sprintf("%02s", 2), "_mesh.rds")) %>% names()
+# [1] "jiscode"   "long"      "lat"       "order"     "group"    
+# [6] "id"        "city_code" "city_name"
