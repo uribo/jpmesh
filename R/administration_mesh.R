c5e55cdc3730fad156e32ce5a0f6f83d726b0e9f
Shinya Uryu <suika1127@gmail.com>
2018-08-05 06:08:33 +0900

    Close #26 [ci skip]

--- R/administration_mesh.R
+++ R/administration_mesh.R
@@ -14,14 +14,35 @@
 #' @export
 administration_mesh <- function(code, type = c("city", "prefecture")) {
 
-  . <- city_code <- meshcode <- NULL
+  rlang::arg_match(type)
+  
+  . <- city_code <- NULL
+  
+  # [todo] replace to jpndistrict:::admins_code_validate in the future
+  if (code[1] %>% purrr::map_lgl(
+    purrr::negate(~ nchar(.x) %in% c(1, 2, 5))) == TRUE) {
+    rlang::abort("Input code must to 2 or 5 digits.")
+  }
+  
+  if (nchar(code[1]) == 1)
+    code <- 
+      sprintf("%02d", as.numeric(code[1]))
+
+  if (nchar(code[1]) == 2) 
+    code_nchar <- 2
+  if (nchar(code[1]) == 5)
+    code_nchar <- 5
+
+  if (code_nchar == 2) 
+    if (code[1] %in% sprintf("%02d", seq(1, 47, by = 1)) == FALSE)
+      rlang::abort("Input prefecture code must to range from 1 to 47.")
   
   if (type == "prefecture") {
     df_city_mesh$meshcode <- substr(df_city_mesh$meshcode, 1, 6)
   }
-
+  
   target <- 
-    paste(sprintf(paste0("%0", nchar(code), "d"), as.numeric(code)), collapse = "|")
+    paste(sprintf(paste0("%0", code_nchar, "d"), as.numeric(code)), collapse = "|")
   
   subset(df_city_mesh, 
          grepl(paste0("^(", target, ")"), 
--- tests/testthat/test-administration_mesh.R
+++ tests/testthat/test-administration_mesh.R
@@ -6,7 +6,19 @@ test_that("multiplication works", {
   expect_named(res, c("meshcode", "geometry"))
   expect_s3_class(res, c("sf"))
   expect_s3_class(res, c("tbl_df"))
+
+  res <- administration_mesh(code = 8, type = "prefecture")
+  expect_gt(nrow(res), 0)
   
+  expect_identical(
+    administration_mesh(code = 8, type = "prefecture"),
+    administration_mesh(code = "8", type = "prefecture")
+  )
+  expect_identical(
+    administration_mesh(code = "08", type = "prefecture"),
+    administration_mesh(code = "8", type = "prefecture")
+  )
+    
   res <- administration_mesh(code = c(35), type = "prefecture")
   expect_equal(dim(res), c(108, 2))
   
@@ -20,4 +32,15 @@ test_that("multiplication works", {
   res <- 
     administration_mesh(code = c("08220", "08221"), type = "city")
   expect_equal(dim(res), c(461, 2))
+})
+
+test_that("Failed patterns", {
+  
+  expect_error(
+    administration_mesh(code = 123)
+  )
+  expect_error(
+    administration_mesh(code = c("08220", "08221"), type = "cities")
+  )
+  
 })
