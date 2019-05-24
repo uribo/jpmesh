f7ffe7ea8aa78f974b87e9deb9723f85d4ba46ca
Shinya Uryu <suika1127@gmail.com>
2019-05-24 23:16:08 +0900

    Tweak

--- tic.R
+++ tic.R
@@ -3,6 +3,12 @@ do_package_checks(error_on = "error")
 get_stage("install") %>%
   add_code_step(install.packages("lwgeom", configure.args = "--without-liblwgeom"))
 
-if (ci_on_travis()) {
-  do_pkgdown(path = ".", branch = "gh-pages")
+if (Sys.getenv("id_rsa") != "") {
+  
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+  
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown(document = FALSE)) %>%
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
