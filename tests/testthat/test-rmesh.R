2072b67a90fb3afda7b3d5090b8ad3dd7dbe0d87
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-05-29 15:29:24 +0900

    Add test

--- tests/testthat/test-rmesh.R
+++ tests/testthat/test-rmesh.R
@@ -6,4 +6,5 @@ test_that(
     res <- rmesh(1, mesh_size = 1.000)
     if (getRversion() >= "3.6.0") expect_equal(res, "64394175")
     else expect_equal(res, "50304610")
+    expect_error(rmesh(1, mesh_size = 0.5))
 })
