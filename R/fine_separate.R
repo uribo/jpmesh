48abd280c2173ae76e1d1fad808b7fb0f8ba2693
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-05-29 17:05:26 +0900

    Update

--- R/fine_separate.R
+++ R/fine_separate.R
@@ -15,19 +15,19 @@
 #' @export
 fine_separate <- function(meshcode = NULL, ...) {
   if (is.mesh(meshcode))
-  mesh_length <- nchar(meshcode)
+    mesh_length <- nchar(meshcode)
   res <- if (mesh_length == 4) {
     res <- paste0(meshcode,
-                rep(0:7, each = 8),
-                rep(0:7, times = 8))
+                  rep(seq.int(0, 7), each = 8),
+                  rep(seq.int(0, 7), times = 8))
   } else if (mesh_length == 6) {
     res <- paste0(meshcode,
-                  rep(0:9, each = 10),
-                  rep(0:9, times = 10))
-    } else if (mesh_length >= 8 & mesh_length <= 10) {
-      res <- paste0(meshcode, 1:4)
+                  rep(seq.int(0, 9), each = 10),
+                  rep(seq.int(0, 9), times = 10))
+  } else if (mesh_length >= 8 & mesh_length <= 10) {
+    res <- paste0(meshcode, seq_len(4))
   } else {
-    rlang::inform("A value greater than the supported mesh size was inputed.")
+    rlang::inform("A value greater than the supported mesh size was inputed.") # nolint
     NA_character_
   }
   return(res)
--- R/mesh_viewer.R
+++ R/mesh_viewer.R
@@ -2,11 +2,9 @@
 #' 
 #' @description Shiny gadgets for jpmesh.
 #' @param ... other parameters
-#' @import shiny
-#' @import miniUI
-#' @import leaflet
-#' @importFrom purrr pmap
-#' @importFrom sf st_sf
+#' @importFrom shiny dialogViewer runGadget selectInput textInput
+#' @importFrom miniUI gadgetTitleBar miniContentPanel miniTabstripPanel miniTabPanel miniPage 
+#' @importFrom leaflet addTiles addPolygons leaflet leafletOutput renderLeaflet
 #' @examples 
 #' \dontrun{
 #' mesh_viewer()
@@ -20,26 +18,26 @@ mesh_viewer <- function(...) {
     miniUI::gadgetTitleBar("Mesh Viewer"),
     miniUI::miniTabstripPanel(
       miniUI::miniTabPanel("Map", icon = shiny::icon("map-o"),
-                   shiny::textInput("lng", "Longitude: ", value = 141.3438),
-                   shiny::textInput("lat", "Latitude: ", value = 43.0625),
-                   shiny::selectInput("mesh_size",
-                                      label = "Select Mesh Size",
-                                      choices = c("80km", "10km", "1km",
-                                                  "500m", "250m", "125m"),
-                                      selected = "1km"),
-                   miniUI::miniContentPanel(padding = 0,
-                                    leaflet::leafletOutput("my.map", height = "100%") # nolint
-                   ))))
+                           shiny::textInput("lng", "Longitude: ", value = 141.3438),
+                           shiny::textInput("lat", "Latitude: ", value = 43.0625),
+                           shiny::selectInput("mesh_size",
+                                              label = "Select Mesh Size",
+                                              choices = c("80km", "10km", "1km",
+                                                          "500m", "250m", "125m"),
+                                              selected = "1km"),
+                           miniUI::miniContentPanel(padding = 0,
+                                                    leaflet::leafletOutput("my_map", height = "100%") # nolint
+                           ))))
   # Server ------------------------------------------------------------------
   server <- function(input, output, session) {
-    output$my.map <- leaflet::renderLeaflet({
-    d <- coords_to_mesh(as.numeric(input$lng),
-                        as.numeric(input$lat),
-                        mesh_size = input$mesh_size) %>%
+    output$my_map <- leaflet::renderLeaflet({
+      d <- coords_to_mesh(as.numeric(input$lng),
+                          as.numeric(input$lat),
+                          mesh_size = input$mesh_size) %>%
         export_meshes()
-    leaflet::leaflet() %>%
-      leaflet::addTiles() %>%
-      leaflet::addPolygons(data = d)
+      leaflet::leaflet() %>%
+        leaflet::addTiles() %>%
+        leaflet::addPolygons(data = d)
     })
   }
   shiny::runGadget(ui,
