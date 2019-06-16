20aefbcd8a5f0d39bbf43104e5de14c12ee1e0f0
Shinya Uryu <suika1127@gmail.com>
2019-06-16 12:43:53 +0900

    Add meshcode_sf

--- tests/testthat/test-export_mesh.R
+++ tests/testthat/test-export_mesh.R
@@ -5,3 +5,12 @@ test_that("typeof", {
   expect_type(export_mesh(53375084), "list")
   expect_equal(sf::st_crs(export_mesh(sample(meshcode_set(80), 1)))$epsg, 4326) # nolint
 })
+
+test_that("Convert include meshcode dataframe to sf", {
+  d <- data.frame(id = seq.int(4),
+              meshcode = rmesh(4),
+              stringsAsFactors = FALSE)
+  res <- meshcode_sf(d, meshcode)
+  expect_s3_class(res, "sf")
+  expect_named(res, c("id", "meshcode", "geometry"))
+})
