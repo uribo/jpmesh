86cde426a8f016f6deb24cbdb5a61ecdb0e781b6
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-11-26 17:02:17 +0900

    Tweaked

--- tests/testthat/test-lint.R
+++ tests/testthat/test-lint.R
@@ -2,6 +2,7 @@ context("Style")
 
 test_that("Package Style", {
   skip_if_not_installed("lintr")
+  skip_on_travis()
   if (requireNamespace("lintr", quietly = TRUE)) {
     lintr::expect_lint_free()
   }
